1ef3dc2d3a8c29adfadb06cdbb42ba4fc15e2b44
Jophine07 <jophinepaul@gmail.com>
2024-06-07 14:34:05 +0530

    Student app Completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    return (
+        
+        <div>
+<NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                            <div class="card" >
+                                    <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Vivek</h5>
+                                        <p class="card-text">my name is vivek coming from Kalady</p>
+                                        <a href="#" class="btn btn-primary">Click here</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                
+                                <div class="card" >
+                                        <img src="https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/cristiano-ronaldo-childhood-photo.jpg?zoom=1.25&w=680&ssl=1" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Vivek</h5>
+                                            <p class="card-text">my name is vivek coming from Kalady</p>
+                                            <a href="#" class="btn btn-primary">Click here</a>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewAll
